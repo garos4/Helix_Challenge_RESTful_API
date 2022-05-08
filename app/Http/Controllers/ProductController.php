4a025a6f37eba26a371e5f3621162000c14583f7
unknown <benmba4@gmail.com>
2022-05-08 15:44:45 +0000

    Added validation trait

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,19 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Traits\Response;
+use App\Traits\ValidationEngine;
 use Exception;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    use Response;
+    use Response, ValidationEngine;
+
+
+
+
+    private $product_validation_rules = [
+        'name' => ['required'],
+        'description' => ['required'],
+        'price' => ['required'],
+        'image' => ['required', 'mimes:jpeg,png,bmp'],
+    ];
+
+
 
     /**
      *  list of all products
      *
      * 
      */
-
     public function index()
     {
         try {
@@ -38,19 +50,30 @@ class ProductController extends Controller
         return $this->successResponse($data);
     }
 
-    
+
     //delete product
     public function destroy($id)
     {
-	
-		if (!$data = Product::find($id)) {
-			return $this->notFoundResponse();
-		}
-	
-		$data->delete();
 
-		return $this->deleteResponse('Product deleted');
+        if (!$data = Product::find($id)) {
+            return $this->notFoundResponse();
+        }
+
+        $data->delete();
+
+        return $this->deleteResponse('Product deleted');
     }
 
+
+    //create a new product
+    public function store(Request $request)
+    {
+        
+        $this->validator($request->all(),$this->product_validation_rules)->validate();
+
+    }
+
+    
+    
 
 }
