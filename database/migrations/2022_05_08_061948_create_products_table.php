bd1009af506f356583a7dc68eb9b9fbd43f5da57
unknown <benmba4@gmail.com>
2022-05-08 07:11:29 +0000

    set up products migration file

--- database/migrations/2022_05_08_061948_create_products_table.php
+++ database/migrations/2022_05_08_061948_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->decimal('price');
+            $table->string('image_link');
             $table->timestamps();
         });
     }
